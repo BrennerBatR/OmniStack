cf51422e9fa88e6ba7261e3712ea09cb330185c7
Brennerbbr <brenner.batista@clubpetro.com.br>
2019-08-20 15:00:59 -0300

    app na metade

---
+++ mobile/src/index.js
@@ -0,0 +1,29 @@
+
+import React, { Fragment } from 'react';
+
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Login />
+  );
+};
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    display: 'flex',
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  text: {
+    fontWeight:'bold',
+    color:'#FFF',
+    fontSize: 20
+  }
+})
+*/
+export default App;
